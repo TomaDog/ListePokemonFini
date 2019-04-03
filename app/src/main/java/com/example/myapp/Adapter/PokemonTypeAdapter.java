45788215b2eea0e9eaf58bc42c562d910d368d56
TomaDog <dognon@et.esiea.fr>
2019-04-03 17:46:38 +0200

    prefinal

---
+++ app/src/main/java/com/example/myapp/Adapter/PokemonTypeAdapter.java
@@ -0,0 +1,71 @@
+package com.example.myapp.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapp.Common.Common;
+import com.example.myapp.Interface.IItemClickListener;
+import com.example.myapp.R;
+import com.robertlevonyan.views.chip.Chip;
+import com.robertlevonyan.views.chip.OnChipClickListener;
+
+import java.util.List;
+
+public class PokemonTypeAdapter extends RecyclerView.Adapter<PokemonTypeAdapter.MyViewHolder> {
+    Context context;
+    List<String> typeList;
+
+    public PokemonTypeAdapter(Context context, List<String> typeList) {
+        this.context = context;
+        this.typeList = typeList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.chip_item,parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.chip.setChipText(typeList.get(position));
+        holder.chip.changeBackgroundColor(Common.getColorByType(typeList.get(position)));
+        holder.setiItemClickListener(new IItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return typeList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        Chip chip;
+        IItemClickListener iItemClickListener;
+
+        public void setiItemClickListener(IItemClickListener iItemClickListener) {
+            this.iItemClickListener = iItemClickListener;
+        }
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            chip = (Chip)itemView.findViewById(R.id.chip);
+            chip.setOnChipClickListener(new OnChipClickListener() {
+                @Override
+                public void onChipClick(View v) {
+                    iItemClickListener.onClick(v,getAdapterPosition());
+                }
+            });
+        }
+    }
+}

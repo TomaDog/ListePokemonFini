2516a82fa0183f53e89410e42cb9a15dd360a667
TomaDog <dognon@et.esiea.fr>
2019-12-19 14:25:27 +0100

    First step

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -13,12 +15,24 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.myapp.Adapter.PokemonListAdapter;
 import com.example.myapp.Common.Common;
 import com.example.myapp.Model.Pokemon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private EditText txtInput;
+
 
     Toolbar toolbar;
 
@@ -79,8 +93,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listv);
+        String [] fragment_pokemon_list ={} ;
+        arrayList = new ArrayList<>(Arrays.asList(fragment_pokemon_list));
+        adapter = new ArrayAdapter<String>(this,R.layout.pokemon_list_item,R.id.txt_pokemon_name,arrayList);
+        listView.setAdapter(adapter);
+        txtInput = (EditText)findViewById(R.id.txtinput);
+        Button btAdd = (Button)findViewById(R.id.btadd);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem=txtInput.getText().toString();
+                arrayList.add(newItem);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle("POKEMON LIST");
